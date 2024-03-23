bd77b9088728dc1ec65dbf8698126a5b067b0a38
Muhammad Salman <muhammadsalman.webdev@gmail.com>
2024-03-23 23:15:00 +0500

    Gallery Section Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,28 @@ SectBtns.forEach((btn, i) => {
     document.querySelector("#menu_res_checkbox").checked = false;
   });
 });
+
+// --------
+
+const galleryBtns = document.querySelectorAll(
+  "button.filter-button[data-filter]"
+);
+const galleryImges = document.querySelectorAll("div.gallery_product");
+
+galleryBtns.forEach((_btn, _i) => {
+  _btn.addEventListener("click", (e) => {
+    galleryBtns.forEach((__btn) => __btn.classList.remove("_active"));
+    _btn.classList.add("_active");
+    if (_i == 0) {
+      galleryImges.forEach((_img, i) => {
+        if (i > 5) _img.classList.add("cd-none");
+      });
+    } else {
+      galleryImges.forEach((_img, _i) => {
+        _img.classList.remove("cd-none");
+      });
+    }
+  });
+
+  if (_i == 0) _btn.click();
+});
